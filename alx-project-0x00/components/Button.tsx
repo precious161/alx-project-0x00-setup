fe5f659384c311e4a9d04f4419e1c603744031cb
feyruza dawud <feyruza.dawud-ug@aau.edu.et>
2025-07-27 01:43:34 +0300

    Added buttons

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return <button className={styles}>{title}</button>;
+};
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,13 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Small Buttn" styles="text-sm px-3 py-1 rounded-sm" />
+      <Button title="Medium Button" styles="text-base px-4 py-2 rounded-md" />
+      <Button title="Large Button" styles="text-lg px-6 py-3 rounded-full" />
     </div>
   );
 };
